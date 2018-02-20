7ac92cc80b98eb7ab00ea6e264a904e70ad526f7
ahmed3ssem <a7med.assem@gmail.com>
2018-02-20 11:46:29 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ahmedassem/exx/data/remote/ApiUtils.java
@@ -0,0 +1,17 @@
+package com.example.ahmedassem.exx.data.remote;
+
+/**
+ * Created by Chike on 12/4/2016.
+ */
+
+public class ApiUtils {
+
+    private ApiUtils() {}
+
+    public static final String BASE_URL = "https://api.stackexchange.com/2.2/";
+
+    public static SOService getSOService() {
+
+        return RetrofitClient.getClient(BASE_URL).create(SOService.class);
+    }
+}
---
+++ app/src/main/java/com/example/ahmedassem/exx/data/remote/SOService.java
@@ -0,0 +1,26 @@
+package com.example.ahmedassem.exx.data.remote;
+
+import com.example.ahmedassem.exx.data.model.SOAnswersResponse;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+/**
+ * Created by Chike on 12/3/2016.
+ */
+
+public interface SOService {
+
+    @GET("/answers?order=desc&sort=activity&site=stackoverflow")
+    Call<SOAnswersResponse> getAnswers();
+
+    // RxJava
+    // Observable<SOAnswersResponse> getAnswers();
+
+    @GET("/answers?order=desc&sort=activity&site=stackoverflow")
+    Call<List<SOAnswersResponse>> getAnswers(@Query("tagged") String tags);
+
+}
